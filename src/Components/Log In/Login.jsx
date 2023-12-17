48f6695ed13e9ff69bf440773b99f10494dc6cc8
Hunter <muhammadhassanmughal47@gmail.com>
2023-12-17 12:38:41 +0500

    first commit

---
+++ src/Components/Log In/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="container">
+      <div className="container-box">
+        <form>
+          <h1>Login</h1>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
