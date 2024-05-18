7111ffc566bf04b01b22e9a2fb24bde7faab95d1
Hunter <muhammadhassanmughal47@gmail.com>
2024-05-18 16:30:49 +0500

    hehe

--- src/Components/Log In/Login.jsx
+++ src/Components/Log In/Login.jsx
@@ -7,13 +7,10 @@ const Login = () => {
       <div className="container-box">
         <form>
           <h1>Login</h1>
-<<<<<<< HEAD
           <label className="login-label">Email: </label>
           <input className="login-input" type="email" name="email" /><br/>
           <label className="login-label">Password: </label>
           <input className="login-input" type="password" name="password"/><br/>
-=======
->>>>>>> origin/main
         </form>
       </div>
     </div>
